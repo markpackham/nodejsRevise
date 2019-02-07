f39566720fe31ba657e5d8c76c8d49c7284300a7
Mark Packham <markpackham1@gmail.com>
2019-02-07 21:05:16 +0000

    move onto events https://youtu.be/fBNz5xF-Kx4?t=2831

--- person.js
+++ person.js
@@ -1,10 +1,18 @@
+// Module Wrapper Function
+// (function (exports, require, module, __filename, __dirname) {
+
+// })
+
+// console.log(__dirname, __filename);
+
 class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
+
   greeting() {
-    console.log(`Name is ${this.name} & I am ${this.age}`);
+    console.log(`Hi I am ${this.name} and my age is ${this.age}`);
   }
 }
 
